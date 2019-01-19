cc6b8960db235aabe324c507a678246e9a71a0fb
Kannan Goundan <kannan@cakoose.com>
2019-01-19 03:04:53 -0800

    Separate charts for encode/decode.
    Also reworked the benchmark charting code a bit.

--- test/bench.js
+++ test/bench.js
@@ -5,58 +5,84 @@ const benchmark = require('benchmark');
 
 const base64 = require('../');
 
-const headers = [
-  'Buffer length',
-  'Buffer encode string',
-  'Buffer decode string',
-  'Buffer encode Buffer',
-  'Buffer decode Buffer',
-  '64 encode',
-  '64 decode'
-];
-
 const powersOfTwo = Array(12)
   .fill()
   .map(function (_, i) {
     return Math.pow(2, i + 6);
   });
 
-const results = [
-  ['Buffer length', 'Buffer encode string', 'Buffer decode string', 'Buffer encode Buffer', 'Buffer decode Buffer', '64 encode', '64 decode'],
-]
+const encodeResults = [
+  [
+    'Buffer length',
+    'Node (to string)',
+    'Node (to Buffer)',
+    '64.encode'
+  ]
+];
+
+const decodeResults = [
+  [
+    'Buffer length',
+    'Node (from string)',
+    'Node (from Buffer)',
+    '64.decode'
+  ]
+];
 
 powersOfTwo.forEach(function (length, i) {
-  const resultsForLength = [length];
-  results.push(resultsForLength);
-
   const plain = crypto.randomBytes(length);
   const encoded = base64.encode(plain);
   const encodedString = encoded.toString('ascii');
 
+  console.log('length=' + length);
+
+  const encodeResult = [length];
+  encodeResults.push(encodeResult);
   (new benchmark.Suite())
-    .add('Buffer encode string', function () {
+    .add('Node (to string)', function () {
       plain.toString('base64');
     })
-    .add('Buffer encode Buffer', function () {
+    .add('Node (to Buffer)', function () {
       Buffer.from(plain.toString('base64'), 'ascii');
     })
-    .add('64 encode', function () {
+    .add('64.encode', function () {
       base64.encode(plain);
     })
-    .add('Buffer decode string', function () {
+    .on('cycle', function (event) {
+      console.log(event.target.toString());
+      encodeResult.push(Math.floor(event.target.hz));
+    })
+    .run();
+
+  const decodeResult = [length];
+  decodeResults.push(decodeResult);
+  (new benchmark.Suite())
+    .add('Node (from string)', function () {
       Buffer.from(encodedString, 'base64');
     })
-    .add('Buffer decode Buffer', function () {
+    .add('Node (from Buffer)', function () {
       Buffer.from(encoded.toString('ascii'), 'base64');
     })
-    .add('64 decode', function () {
+    .add('64.decode', function () {
       base64.decode(encoded);
     })
     .on('cycle', function (event) {
       console.log(event.target.toString());
-      resultsForLength.push(Math.floor(event.target.hz));
+      decodeResult.push(Math.floor(event.target.hz));
     })
     .run();
 });
 
-console.log(results);
+function writeResultsArray (fieldName, resultsArray) {
+  console.log('  ' + fieldName + ': [');
+  resultsArray.forEach(function (row) {
+    console.log('    ' + JSON.stringify(row) + ',');
+  });
+  console.log('  ],');
+}
+
+console.log('Content for "test/bench-results.js":');
+console.log('var benchResults = {');
+writeResultsArray('encode', encodeResults);
+writeResultsArray('decode', decodeResults);
+console.log('};');
