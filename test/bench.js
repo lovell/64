050a4c70e9ec57b1b39e5e6da87eedea36f25f1a
Kannan Goundan <kannan@cakoose.com>
2019-01-18 16:29:44 -0800

    bench.js: Include Node results without the extra string conversion
    I was surprised when I saw the extra string conversion in the benchmark
    code.  I think it may surprise others as well.
    
    Though people who care about performance will probably be using Buffers
    as input and output, I think it's worth making it clearer what the
    benchmark is actually measuring.
    
    It also helps explain why the performance difference is so large.

--- test/bench.js
+++ test/bench.js
@@ -5,12 +5,15 @@ const benchmark = require('benchmark');
 
 const base64 = require('../');
 
-const mapTestNameToResultsColumn = {
-  'Buffer encode': 1,
-  'Buffer decode': 2,
-  '64 encode': 3,
-  '64 decode': 4
-};
+const headers = [
+  'Buffer length',
+  'Buffer encode string',
+  'Buffer decode string',
+  'Buffer encode Buffer',
+  'Buffer decode Buffer',
+  '64 encode',
+  '64 decode'
+];
 
 const powersOfTwo = Array(12)
   .fill()
@@ -18,30 +21,32 @@ const powersOfTwo = Array(12)
     return Math.pow(2, i + 6);
   });
 
-const results = Array(powersOfTwo.length);
+const results = [
+  ['Buffer length', 'Buffer encode string', 'Buffer decode string', 'Buffer encode Buffer', 'Buffer decode Buffer', '64 encode', '64 decode'],
+]
 
 powersOfTwo.forEach(function (length, i) {
-  results[i] = Array(5);
-  results[i][0] = length;
+  const resultsForLength = [length];
+  results.push(resultsForLength);
 
   const plain = crypto.randomBytes(length);
   const encoded = base64.encode(plain);
+  const encodedString = encoded.toString('ascii');
 
   (new benchmark.Suite())
-    .add('Buffer encode', function () {
+    .add('Buffer encode string', function () {
+      plain.toString('base64');
+    })
+    .add('Buffer encode Buffer', function () {
       Buffer.from(plain.toString('base64'), 'ascii');
     })
     .add('64 encode', function () {
       base64.encode(plain);
     })
-    .on('cycle', function (event) {
-      console.log(event.target.toString());
-      results[i][mapTestNameToResultsColumn[event.target.name]] = Math.floor(event.target.hz);
+    .add('Buffer decode string', function () {
+      Buffer.from(encodedString, 'base64');
     })
-    .run();
-
-  (new benchmark.Suite())
-    .add('Buffer decode', function () {
+    .add('Buffer decode Buffer', function () {
       Buffer.from(encoded.toString('ascii'), 'base64');
     })
     .add('64 decode', function () {
@@ -49,7 +54,7 @@ powersOfTwo.forEach(function (length, i) {
     })
     .on('cycle', function (event) {
       console.log(event.target.toString());
-      results[i][mapTestNameToResultsColumn[event.target.name]] = Math.floor(event.target.hz);
+      resultsForLength.push(Math.floor(event.target.hz));
     })
     .run();
 });
